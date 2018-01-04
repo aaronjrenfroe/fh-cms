79da124c09b0360722800f1feb23cf0856010480
Aaron <afterrabble@gmail.com>
2018-01-03 17:34:16 -0800

    fixed auth navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,60 @@
+import { TokenInterceptor } from './services/token-interceptor';
+import { PostsComponent } from './components/posts/posts.component';
+import { UsersComponent } from './components/users/users.component';
+// Global imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
-
+// local imports
 import { AppComponent } from './app.component';
+import { UserService } from './services/user.service';
+import { PostService } from './services/post.service';
+import { AuthService } from './services/auth.service';
+import { AdminAuthGuard } from './services/admin-auth-guard.service';
+import { AuthGuard } from './services/auth-guard.service';
+import { LoginComponent } from './components/login/login.component';
+import { NoAccessComponent } from './components/no-access/no-access.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    UsersComponent,
+    LoginComponent,
+    NoAccessComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: PostsComponent, canActivate: [AuthGuard] },
+      { path: 'users', component: UsersComponent, canActivate: [AuthGuard, AdminAuthGuard] },
+      { path: 'login', component: LoginComponent },
+      { path: 'no-access', component: NoAccessComponent }
+    ])
   ],
-  providers: [],
+  providers: [
+    UserService,
+    PostService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    },
+    AuthService,
+    AdminAuthGuard,
+    AuthGuard
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
