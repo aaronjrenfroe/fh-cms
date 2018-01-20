be721467c3a2d602477c6bfbe305dbe0f4a0d9ea
Aaron <afterrabble@gmail.com>
2018-01-20 15:17:30 -0800

    fix2

--- src/app/common/app-error-handler.ts
+++ src/app/common/app-error-handler.ts
@@ -1,10 +1,9 @@
 import { ErrorHandler } from "@angular/core/src/error_handler";
-import { error } from "selenium-webdriver";
 
 export class AppErrorHandler implements ErrorHandler{
 
     handleError(error){
         // Do something
-        alert('An Unexpected error occured' );
+        alert('An Unexpected error occured '+ error);
     }
 }
