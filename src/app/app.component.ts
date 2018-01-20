3c56e6341d987319f8ddc9a82a2ba8ccc5ab177b
Aaron <afterrabble@gmail.com>
2018-01-20 15:25:38 -0800

    private to public in component constructors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from './services/auth.service';
 export class AppComponent {
   title = 'app';
   
-  constructor(private auth: AuthService){
+  constructor(public auth: AuthService){
 
   }
 
